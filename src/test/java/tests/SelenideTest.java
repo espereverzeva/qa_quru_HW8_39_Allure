1a3d5c63bad26ea41c329e107498fab28d426596
Your Name <you@example.com>
2026-01-28 22:42:12 +0700

    fix

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -1,9 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
 
 public class SelenideTest {
 
@@ -12,5 +15,10 @@ public class SelenideTest {
 
         open("https://github.com");
         $(".search-input").click();
+        $("#query-builder-test").sendKeys("espereverzeva/qa_quru_HW8_39_Allure");
+        $("#query-builder-test").submit();
+        $(linkText("espereverzeva/qa_quru_HW8_39_Allure")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
     }
 }
